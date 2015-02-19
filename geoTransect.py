2799d2ff9a30ccc568d6f7c726fa391f39e969aa
ben-bougher <ben.bougher@gmail.com>
2015-02-19 09:35:21 -0800

    fixed some seismic coord scaling

--- geoTransect.py
+++ geoTransect.py
@@ -1,15 +1,16 @@
 from dataContainers import transectContainer
 
 ## These will be read from a config file .... ##
-transect_file = 'data/transectTest/all'
+transect_file = 'data/transect_shp'
 seismic_dir = 'data/seismic/'
-las_file = 'data/wells/P-111/wireline_log/P-111_out.LAS'
+las_shape = 'data/wells/'
 elevation_file = 'data/elevation_raster/NS_DEM_ascii.asc'
 extents = [0,100000, -10000,0]
 
 # Initial the main object
 tc = transectContainer(transect_file, seismic_dir, elevation_file,
-                       las_file, extents)
+                       las_shape, extents)
+
 
 tc.plot()
 
